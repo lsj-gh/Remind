ab9d64651ccda712e5815dd974295a40d6b099a6
betta- <2463963246@qq.com>
2017-05-23 13:58:36 +0800

    add toast and log

---
+++ app/src/main/java/com/example/administrator/mapleone/activity/MyRunnableActivity.java
@@ -0,0 +1,55 @@
+package com.example.administrator.mapleone.activity;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.administrator.mapleone.R;
+import com.example.administrator.mapleone.utils.Myshow;
+
+public class MyRunnableActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView;
+    private static final String TAG = "MyRunnableActivity";
+    public static final int UPDATA_TEXT_RUN = 1;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Myshow.d(TAG,"go into handleMessage");
+            switch (msg.what) {
+                case UPDATA_TEXT_RUN :
+                    Myshow.d(TAG,"go into UPDATA_TEXT_RUN");
+                    textView.setText("obtainMessage");
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_runnable);
+        Button button = (Button) findViewById(R.id.bt_run);
+        textView = (TextView) findViewById(R.id.tv_run);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                handler.obtainMessage(UPDATA_TEXT_RUN).sendToTarget();
+                Myshow.d(TAG,"obtainMessage");
+//                Message message = new Message();
+//                message.what = UPDATA_TEXT_RUN;
+//                handler.sendMessage(message);
+//                Myshow.d(TAG,"obtainMessage");
+            }
+        }).start();
+    }
+}
