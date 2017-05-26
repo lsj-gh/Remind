9a509ffea66aae6281783729680e98b3f86fba60
betta- <2463963246@qq.com>
2017-05-26 16:42:28 +0800

    绑定服务，正常的启动服务已注释

--- app/src/main/java/com/example/administrator/mapleone/activity/MyServiceActivity.java
+++ app/src/main/java/com/example/administrator/mapleone/activity/MyServiceActivity.java
@@ -1,6 +1,9 @@
 package com.example.administrator.mapleone.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +13,8 @@ import com.example.administrator.mapleone.R;
 import com.example.administrator.mapleone.service.MyService;
 
 public class MyServiceActivity extends AppCompatActivity implements View.OnClickListener {
-    private MyService myService = new MyService();
+//    private MyService myService = new MyService();
+    private MyService myService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +25,32 @@ public class MyServiceActivity extends AppCompatActivity implements View.OnClick
         startButton.setOnClickListener(this);
         stopButton.setOnClickListener(this);
     }
-
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.MyBinder binder = (MyService.MyBinder) service;
+            myService = binder.getService();
+            myService.showLog("onServiceConnected");
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            myService.showLog("onServiceDisconnected");
+        }
+    };
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_start:
                 Intent startIntent = new Intent(this, MyService.class);
                 startIntent.putExtra("name", "wayslde");
-                startService(startIntent);
+//                startService(startIntent);
+                bindService(startIntent,connection,BIND_AUTO_CREATE);
                 break;
             case R.id.bt_stop:
 //                Intent stopIntent = new Intent(this, MyService.class);
 //                stopService(stopIntent);
-                myService.showLog();
+//                myService.showLog();
+                unbindService(connection);
                 break;
         }
     }
--- app/src/main/java/com/example/administrator/mapleone/service/MyService.java
+++ app/src/main/java/com/example/administrator/mapleone/service/MyService.java
@@ -2,6 +2,7 @@ package com.example.administrator.mapleone.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 
@@ -17,7 +18,8 @@ public class MyService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return new MyBinder();
     }
 
     @Override
@@ -40,8 +42,20 @@ public class MyService extends Service {
         Myshow.d(TAG, "MyService-------------onDestroy()");
     }
 
-    public void showLog() {
+    public class MyBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    public void showLog(String s) {
 //        Myshow.d(TAG, "showLog()");
-        Myshow.d(TAG, logMessage);
+//        Myshow.d(TAG, logMessage);
+        Myshow.d(TAG, s);
     }
 }
